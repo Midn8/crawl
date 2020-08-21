60eb3143ebc30c8a444dbdff880c53c7f9b2088a
Nicholas Feinberg <pleasingfung@gmail.com>
2020-08-21 12:57:53 -0700

    New monster: bloated husk
    Intended for use as an early Swamp enemy, bloated husks charge at
    the player at 14 speed and deliver a weakish melee attack. When
    killed, they explode, dealing 8d8 physical damage to everything in
    radius 2. They're a bit similar to lurking horrors (but much simpler
    and easier to deal with) or to ballistomycetes (but with actual HP
    and without the very complex spore mechanics).
    
    It might be interesting to have their explosion flatten trees, but
    maybe that's just complexity for the sake of it.

--- crawl-ref/source/monster-type.h
+++ crawl-ref/source/monster-type.h
@@ -673,6 +673,8 @@ enum monster_type                      // menv[].type
     MONS_GHOUL,
 #if TAG_MAJOR_VERSION == 34
     MONS_FLAMING_CORPSE,
+#else
+    MONS_BLOATED_HUSK,
 #endif
     MONS_MUMMY,
     MONS_BOG_BODY,
@@ -1068,6 +1070,7 @@ enum monster_type                      // menv[].type
     MONS_MAGGIE,
     MONS_NAMELESS,
     MONS_PALENTONGA,
+    MONS_BLOATED_HUSK,
 #endif
 
     NUM_MONSTERS,               // used for polymorph
