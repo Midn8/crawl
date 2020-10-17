mgdelmonte/crawl1

Tweak swamp monster XP

crawl-ref/source/mon-data.h
@@ -1299,7 +1299,7 @@ DUMMY(MONS_BEAR, 'h', LIGHTGREY, "bear", TILEP_MONS_BLACK_BEAR)
     MONS_SPRIGGAN_RIDER, 'i', LIGHTBLUE, "spriggan rider",
     M_WARM_BLOOD | M_SPEAKS | M_SEE_INVIS | M_FIGHTER | M_FLIES,
     MR_VUL_POISON, // the mount
-    10, MONS_SPRIGGAN, MONS_SPRIGGAN, MH_NATURAL, 100,
+    8, MONS_SPRIGGAN, MONS_SPRIGGAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 27}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     11, 605,
     1, 18, MST_NO_SPELLS, true, S_SHOUT,
@@ -1634,9 +1634,9 @@ DUMMY(MONS_GIANT_LIZARD, 'l', LIGHTGREY, "giant lizard", TILEP_MONS_LEOPARD_GECK
     MONS_BLOATED_HUSK, 'n', YELLOW, "bloated husk",
     M_NO_ZOMBIE,
     MR_RES_COLD,
-    10, MONS_GHOUL, MONS_BLOATED_HUSK, MH_UNDEAD, 40,
+    8, MONS_GHOUL, MONS_BLOATED_HUSK, MH_UNDEAD, 40,
     { { AT_HIT, AF_ROT, 18 }, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
-    8, 500,
+    8, 400,
     5, 5, MST_NO_SPELLS, false, S_SILENT,
     I_HUMAN, HT_AMPHIBIOUS, 14, DEFAULT_ENERGY,
     MONUSE_OPEN_DOORS, SIZE_MEDIUM, MON_SHAPE_HUMANOID,
@@ -2217,7 +2217,7 @@ DUMMY(MONS_SPIDER, 's', CYAN, "spider", TILEP_MONS_REDBACK)
     MONS_ALLIGATOR, 't', LIGHTBLUE, "alligator",
     M_COLD_BLOOD,
     MR_NO_FLAGS,
-    10, MONS_CROCODILE, MONS_ALLIGATOR, MH_NATURAL, 40,
+    7, MONS_CROCODILE, MONS_ALLIGATOR, MH_NATURAL, 40,
     { {AT_BITE, AF_PLAIN, 30}, {AT_TAIL_SLAP, AF_PLAIN, 15}, AT_NO_ATK,
        AT_NO_ATK },
     12, 720,

Midn8/crawl

crawl-ref/source/mon-data.h
@@ -1233,7 +1233,7 @@ DUMMY(MONS_BEAR, 'h', LIGHTGREY, "bear", TILEP_MONS_BLACK_BEAR)
     MONS_SPRIGGAN_RIDER, 'i', LIGHTBLUE, "spriggan rider",
     M_WARM_BLOOD | M_SPEAKS | M_SEE_INVIS | M_FIGHTER | M_FLIES,
     MR_VUL_POISON, // the mount
-    10, MONS_SPRIGGAN, MONS_SPRIGGAN, MH_NATURAL, 100,
+    8, MONS_SPRIGGAN, MONS_SPRIGGAN, MH_NATURAL, 100,
     { {AT_HIT, AF_PLAIN, 27}, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
     11, 605,
     1, 18, MST_NO_SPELLS, true, S_SHOUT,
@@ -1568,9 +1568,9 @@ DUMMY(MONS_GIANT_LIZARD, 'l', LIGHTGREY, "giant lizard", TILEP_MONS_LEOPARD_GECK
     MONS_BLOATED_HUSK, 'n', YELLOW, "bloated husk",
     M_NO_ZOMBIE,
     MR_RES_COLD,
-    10, MONS_GHOUL, MONS_BLOATED_HUSK, MH_UNDEAD, 40,
+    8, MONS_GHOUL, MONS_BLOATED_HUSK, MH_UNDEAD, 40,
     { { AT_HIT, AF_ROT, 18 }, AT_NO_ATK, AT_NO_ATK, AT_NO_ATK },
-    8, 500,
+    8, 400,
     5, 5, MST_NO_SPELLS, false, S_SILENT,
     I_HUMAN, HT_AMPHIBIOUS, 14, DEFAULT_ENERGY,
     MONUSE_OPEN_DOORS, SIZE_MEDIUM, MON_SHAPE_HUMANOID,
@@ -2099,7 +2099,7 @@ DUMMY(MONS_SPIDER, 's', CYAN, "spider", TILEP_MONS_REDBACK)
     MONS_ALLIGATOR, 't', LIGHTBLUE, "alligator",
     M_COLD_BLOOD,
     MR_NO_FLAGS,
-    10, MONS_GIANT_LIZARD, MONS_ALLIGATOR, MH_NATURAL, 40,
+    7, MONS_GIANT_LIZARD, MONS_ALLIGATOR, MH_NATURAL, 40,
     { {AT_BITE, AF_PLAIN, 30}, {AT_TAIL_SLAP, AF_PLAIN, 15}, AT_NO_ATK,
        AT_NO_ATK },
     12, 720,